689d5141c7ba0978e63f6fbd9622d41569e384f7
Speed Wasswa <speedwasswa02@gmail.com>
2025-02-12 16:20:39 +0300

    Laravel-tailwindcss Installed

---
+++ resources/js/Layouts/Default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <Navigation/>
+        <div>
+            <slot/>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+import Navigation from '@/Components/Navigation.vue';
+
+</script>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <Head title="Page Title" />
+    <div class="max-w-7xl mx-auto p-8">
+        Home
+    </div>
+</template>
+
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import Default from '@/Layouts/Default.vue';
+
+defineOptions({layout:Default})
+
+
+defineProps({
+    title:String
+})
+</script>
